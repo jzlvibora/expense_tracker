089bd09f32eb02bd8d09bf511e408e5d6b2848d1
Jayzel Gonzales <j.vbr.gonzales@gmail.com>
2025-01-31 21:28:41 +0800

    WIP: authentication feature

--- src/main/java/com/jvg/ExpenseTracker/model/User.java
+++ src/main/java/com/jvg/ExpenseTracker/model/User.java
@@ -2,6 +2,8 @@ package com.jvg.ExpenseTracker.model;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
@@ -10,7 +12,6 @@ import java.util.Collection;
 import java.util.List;
 
 @Entity
-@Data
 public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -22,6 +23,7 @@ public class User implements UserDetails {
 
     @Column(nullable = false)
     private String email;
+
     @Column(nullable = false)
     private String password;
 
@@ -34,6 +36,10 @@ public class User implements UserDetails {
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return List.of();
+    }
+
+    public String getPassword() {
+        return password;
     }
 
     @Override
@@ -59,5 +65,49 @@ public class User implements UserDetails {
     @Override
     public boolean isEnabled() {
         return true;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public LocalDate getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(LocalDate createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public LocalDate getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDate updatedAt) {
+        this.updatedAt = updatedAt;
     }
 }
